f64aad3b94f21f048e033e7fe0888453518e6692
Krut-in <krutin31@gmail.com>
2025-11-21 15:08:01 -0500

    Initial Commit

---
+++ name/nameApp.swift
@@ -0,0 +1,17 @@
+//
+//  nameApp.swift
+//  name
+//
+//  Created by Krutin Rathod on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct nameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
